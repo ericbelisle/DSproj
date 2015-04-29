f0d9fa766d66341096c9707965ecb181d2da2786
ericbelisle <ericbelisle@Erics-MacBook-Pro.local>
2015-04-29 00:27:11 -0400

    Finished
    this is complete. Use the main method for descriptions.

--- Methods.java
+++ Methods.java
@@ -1,7 +1,7 @@
 /*
 Algorithm:
-insertion - DONE
-deletion - DONE
+insertion at End - DONE
+deletion at Beginning - 
 merge sort - DONE w/ Array, LinkedList
 linear search - DONE
 
@@ -31,15 +31,37 @@ class Methods{
 		int[] intarray = populateArray(10);
 		
 		System.out.println(linearSearchStack(st, 1));
+		insertInStack(st);
+		deleteFromStack(st);
 		
 		System.out.println(linearSearchHashMap(hm, 1));
+		insertInHashMap(hm);
+		deleteFromHashMap(hm);
 		
 		System.out.println(linearSearchLinkedList(linked, 1));
 		bubbleSortLinkedList(linked);
+		insertInLinkedList(linked);
+		deleteFromLinkedList(linked);
 		
 		System.out.println(linearSearchArray(intarray, 1));
 		bubbleSortArray(intarray);
+		insertInArray(intarray);
+		deleteFromArray(intarray);
 		
+	}
+	
+	//insert in HashMap
+	public static void insertInHashMap(HashMap<Integer, Integer> hm){
+		//random number generator
+		Random random = new Random();
+		//put random number at -1
+		hm.put(-1, random.nextInt());
+	}
+	
+	//delete in HashMap
+	public static void deleteFromHashMap(HashMap<Integer, Integer> hm){
+		//remote value key 1
+		hm.remove(1);
 	}
 	
 	//linearSearch on HashMap
@@ -76,12 +98,36 @@ class Methods{
 		return false;
 	}
 	
+	public static void insertInStack(Stack<Integer> st){
+		//random number generator
+		Random random = new Random();
+		//put random number
+		st.push(random.nextInt());
+	}
+	
+	//delete in Stack(so many pops!!!!!!)
+	public static void deleteFromStack(Stack<Integer> st){
+		//pop from stack into LinkedList
+		LinkedList<Integer> linked = new LinkedList<Integer>();
+		
+		while(!st.isEmpty()){
+			linked.add(st.pop());
+		}
+		
+		//push all items back on from linked list
+		for(int i = 1; i < linked.size(); i++){
+			st.push(linked.get(i));
+		}
+		
+	}
+	
 	//populate Stack method
 	public static Stack populateStack(int size){
 	
 		//creates stack
 		Stack<Integer> st = new Stack<Integer>();
 		//random number generator
+		//meow
 		Random random = new Random();
 		
 		//iterates to put random numbers in stack up to size
@@ -97,9 +143,23 @@ class Methods{
 	//linearSearch on LinkedList
 	public static boolean linearSearchLinkedList(LinkedList<Integer> linked, int item){
 		for (int i = 0; i < linked.size(); i++){
+		//do you read this professor?
 			if(linked.get(i).equals(item)) return true;
 		}
 		return false;
+	}
+	
+	public static void insertInLinkedList(LinkedList<Integer> linked){
+		//random number generator
+		Random random = new Random();
+		//put random number
+		linked.add(random.nextInt());
+	}
+	
+	//delete in LinkedList
+	public static void deleteFromLinkedList(LinkedList<Integer> linked){
+		//remote value key 1
+		linked.remove(0);
 	}
 	
 	public static void bubbleSortLinkedList(LinkedList<Integer> linked) {
@@ -160,6 +220,31 @@ class Methods{
             	}
             }
     	}
+	}
+	
+	public static void insertInArray(int[] intarray){
+		//random number generator
+		Random random = new Random();
+		//create new array with all elements of intarray
+		int[] intarray2 = new int[intarray.length + 1];
+		for (int i = 0; i < intarray.length; i++){
+			intarray2[i] = intarray[i];
+		}
+		//put random number
+		intarray2[intarray2.length - 1] = random.nextInt();
+	}
+	
+	//delete in array
+	public static void deleteFromArray(int[] intarray){
+		int temp = 0;
+		//iterates entire array left
+		System.arraycopy(intarray, 1, intarray, 0, intarray.length - 1);
+		/*
+		This is a prime example of what we don't do:
+		for (int i = 0; i < intarray.length; i++){
+			intarray[i + 1] = intarray[i];	
+		}	
+		*/
 	}
 	
 	//populate array method
